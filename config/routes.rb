14b96179a5ab92fc444952d984d106b8a572e028
chiasms <chasms@chas.ms>
2017-03-24 16:30:14 -0400

    added root_path

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new', as: 'login'
   post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#detroy', as: 'logout'
+  delete '/logout', to: 'sessions#destroy', as: 'logout'
 
-  get '/', to: 'sessions#new'
+  get '/', to: 'sessions#new', as: 'root'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
