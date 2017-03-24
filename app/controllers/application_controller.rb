28c04ec885849c1ee5305f2e2623f7a83f41ff6d
chiasms <chasms@chas.ms>
2017-03-24 14:54:55 -0400

    added account validation to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def logged_in?
+    !!User.where(id: session[:user_id]).first
+  end
+
+  def account
+    if logged_in?
+      @account = Account.find(session[:user_id])
+    else
+      if session    # destroy sessions that are no longer linked to user accounts
+        session.destroy
+      end
+      redirect_to login_path    # if not logged in, redirect to login
+    end
+  end
+
 end
